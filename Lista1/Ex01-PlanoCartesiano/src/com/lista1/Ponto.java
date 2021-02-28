20dacc157416e91392e7a340c8e7d47bbab12fc1
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-28 20:05:59 -0300

    Criação da classe Ponto

---
+++ Lista1/Ex01-PlanoCartesiano/src/com/lista1/Ponto.java
@@ -0,0 +1,20 @@
+package com.lista1;
+import java.lang.Math;
+
+public class Ponto {
+    public Ponto (double abscissa, double ordenada) {
+        this.abscissa = abscissa;
+        this.ordenada = ordenada;
+    }
+
+    public double distanciaAte(Ponto p) {
+        double distanciaAbscissas = this.abscissa - p.abscissa;
+        double distanciaOrdenadas = this.ordenada - p.ordenada;
+        double d = Math.sqrt(Math.pow(distanciaAbscissas, 2) + Math.pow(distanciaOrdenadas, 2));
+        return d;
+    }
+
+    private double abscissa;
+    private double ordenada;
+
+}
