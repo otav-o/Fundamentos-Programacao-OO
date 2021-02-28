9e0830d59c1998069288ac0b721d4b3cffa42be6
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-28 20:47:47 -0300

    Enunciado da questão

---
+++ Lista1/Ex03-CompraDeCarro/src/com/lista1/Main.java
@@ -0,0 +1,24 @@
+/*
+    Crie um aplicativo que permita ao consumidor customizar a escolha de um carro
+    novo com seus opcionais, câmbio automático, vidro automático, alarme, teto solar, kit
+    multimídia, potência do motor, dentre outros opcionais.
+
+    Ao final mostre os detalhes do carro escolhido e o custo conforme o cálculo a seguir:
+
+    O preço do carro depende do modelo, acrescido IPI, 20% do custo final do veículo, se
+    for importado ainda é taxado em 30% sobre o preço. Veículos com motores 1.0 o IPI é
+    reduzido a 10%.
+    Cada acessório opcional acrescenta seu custo ao veículo, segue a tabela de preço
+    abaixo:
+    Ar -> R$ 3.000,00, câmbio automático -> R$ 5.000,00, alarme -> R$ 800,00, pintura
+    especial, metálica ou comemorativa -> R$ 2.500,00, teto solar -> R$ 4.000,00 e kit
+    multimidia -> R$ 1.800,00
+ */
+package com.lista1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
