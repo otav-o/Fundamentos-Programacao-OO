99abbbbd82efe4dec0ec7816515ff923d64daab1
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-28 20:14:16 -0300

    Criação do método retornarPonto [Fim do exercício]

--- Lista1/Ex01-PlanoCartesiano/src/com/lista1/Main.java
+++ Lista1/Ex01-PlanoCartesiano/src/com/lista1/Main.java
@@ -7,27 +7,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("com.lista1.Ponto A\n" +
-                "Abscissa: ");
-        double absA = in.nextDouble();
-        System.out.print("Ordenada: ");
-        double ordA = in.nextDouble();
-
-        System.out.print("com.lista1.Ponto B\n" +
-                "Abscissa: ");
-        double absB = in.nextDouble();
-        System.out.print("Ordenada: ");
-        double ordB = in.nextDouble();
-
-        Ponto a = new Ponto(absA, ordA);
-        Ponto b = new Ponto(absB, ordB);
+        Ponto a = retornarPonto("a");
+        Ponto b = retornarPonto("b");
 
         System.out.println("A distância entre os dois pontos é " + a.distanciaAte(b));
         System.out.println("Confirmação da distância: " + b.distanciaAte(a));
 
     }
 
+    public static Ponto retornarPonto(String nome) {
+        Scanner in = new Scanner(System.in);
 
-}
+        System.out.print("Ponto " + nome +"\n" +
+                "Abscissa: ");
+        double abs = in.nextDouble();
+
+        System.out.print("Ordenada: ");
+        double ord = in.nextDouble();
+
+        return new Ponto(abs, ord);
+    }
+} // FIM
