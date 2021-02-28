7bacd5108ec532d0681cb65f819131948ea2d031
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-28 20:33:25 -0300

    Criação de construtor na classe Triangulo

--- Lista1/Ex02-Triangulo/src/com/lista1/Triangulo.java
+++ Lista1/Ex02-Triangulo/src/com/lista1/Triangulo.java
@@ -6,6 +6,10 @@ public class Triangulo {
     private double lado2;
     private double lado3;
 
+    public Triangulo(double l1, double l2, double l3) {
+        lado1 = l1; lado2 = l2; lado3 = l3;
+    }
+
     public double getArea() {
         double p = getPerimetro();
         return Math.sqrt(p * (p-lado1) * (p-lado2) * (p-lado3));
