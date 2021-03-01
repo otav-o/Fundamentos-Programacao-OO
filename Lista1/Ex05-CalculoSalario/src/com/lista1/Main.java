2d60e3f73e422d15cdb5f344647195d172b959de
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-01 06:44:21 -0300

    Enunciado questão 5

---
+++ Lista1/Ex05-CalculoSalario/src/com/lista1/Main.java
@@ -0,0 +1,27 @@
+/*
+    Criar um aplicativo para cálculo do salário líquido em que três valores devem ser
+    informados pelo usuário: a quantidade de horas trabalhadas, o salário-hora e o
+    número de dependentes. O aplicativo deve mostrar em janela de saída as
+    informações conforme abaixo
+
+    Salário bruto = Horas trabalhadas * salário-hora + (50 * número de dependentes)
+    Desconto INSS
+        Se salário bruto <= 1000 INSS=salário bruto * 8.5/100
+        Se salário bruto > 1000 INSS=salário bruto * 9/100
+    Desconto IR
+        Se salário bruto <= 500 IR=0
+        Se salário bruto > 500 e <= 1000 IR=salário bruto*5/100
+        Se salário bruto > 1000 IR=salário bruto*7/100
+    Salário líquido = Salário bruto - INSS – IR
+
+ */
+
+package com.lista1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
