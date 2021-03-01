a23fe0b369d552af0803ebb0b12b3bf6a6a09cd5
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-01 09:42:42 -0300

    Enunciado

---
+++ Lista1/Ex06-CensoDemografico/src/com/lista1/Main.java
@@ -0,0 +1,23 @@
+/*
+    Faça um programa que compile dados acerca de uma população, analisando, para cada indivíduo:
+    - sexo (masculino, feminino)
+    - cor dos olhos (azuis, verdes, castanhos)
+    - cor dos cabelos (louros, castanhos, pretos)
+    - idade.
+    O aplicativo deve calcular e imprimir as seguintes informações:
+    a) a maior e a menor altura dos habitantes;
+    b) a média de altura das mulheres;
+    c) o número de homens;
+    d) A porcentagem de homens e de mulheres.
+    b) A porcentagem de indivíduos do sexo feminino entre 18 e 35 anos
+     e que tenham olhos verdes e cabelos louros
+ */
+
+package com.lista1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
