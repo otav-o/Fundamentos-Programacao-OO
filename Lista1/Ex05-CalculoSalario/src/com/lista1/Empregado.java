b68f294f5cd7a15323d9fdbc7ed73d48b8884418
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-01 07:59:02 -0300

    Finalização da classe Empregado

--- Lista1/Ex05-CalculoSalario/src/com/lista1/Empregado.java
+++ Lista1/Ex05-CalculoSalario/src/com/lista1/Empregado.java
@@ -2,6 +2,15 @@ package com.lista1;
 
 public class Empregado {
 
+    public Empregado(Emprego emprego, int numeroDependentes) {
+        this.emprego = emprego;
+        this.numeroDependentes = numeroDependentes;
+    }
+
+    public double getSalarioLiquido() {
+        return emprego.getSalarioLiquido(this);
+    }
+
     public int getNumeroDependentes() {
         return numeroDependentes;
     }
