dcd0595021b353e0dde17574e125c29117818190
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-01 07:42:28 -0300

    Esboço da classe Empregado

---
+++ Lista1/Ex05-CalculoSalario/src/com/lista1/Empregado.java
@@ -0,0 +1,11 @@
+package com.lista1;
+
+public class Empregado {
+
+    public int getNumeroDependentes() {
+        return numeroDependentes;
+    }
+
+    private Emprego emprego;
+    private int numeroDependentes;
+}
