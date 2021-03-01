8de0853abc761e37df9f95c26728f12a28856bd1
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-01 10:41:09 -0300

    Criação do método maiorAltura()

--- Lista1/Ex06-CensoDemografico/src/com/lista1/Censo.java
+++ Lista1/Ex06-CensoDemografico/src/com/lista1/Censo.java
@@ -6,5 +6,16 @@ public class Censo {
         this.populacao = populacao;
     }
 
+    private Pessoa maiorAltura() {
+
+        Pessoa maisAlta = populacao[0];
+
+        for (int i = 0; i < populacao.length; i++) {
+            if (populacao[i].altura > maisAlta.altura)
+                maisAlta = populacao[i];
+        }
+        return maisAlta;
+    }
+
     private Pessoa[] populacao;
 }
