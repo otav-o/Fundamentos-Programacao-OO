b26239a1314482b5341ff9ca2380a51cdfb6946a
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-02 21:35:28 -0300

    Enunciado

---
+++ src/com/aula2/Main.java
@@ -0,0 +1,20 @@
+/*
+    Uma empresa possui diversos funcionários, cujos salários são calculados da seguinte forma:
+
+    Salário = salário base - 13% de inss + 50 por filho, até o limite de 3 filhos,
+    -3% se o funcionário usa vale transporte
+    +5% se trabalha à noite
+    e +2% do total das vendas, se o funcionário for vendedor.
+
+    Faça um sistema que leia os funcionários da empresa e mostre o total de salários a ser pago pela empresa
+
+ */
+
+package com.aula2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
