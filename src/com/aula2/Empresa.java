2be7f626a4695cd389c25df7f66e8993132c9d62
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-02 22:29:12 -0300

    Criação de construtor na classe Empresa

--- src/com/aula2/Empresa.java
+++ src/com/aula2/Empresa.java
@@ -2,6 +2,10 @@ package com.aula2;
 
 public class Empresa {
 
+    public Empresa (double totalVendas) {
+        this.totalVendas = totalVendas;
+    }
+
     private double totalVendas;
 
     public double getTotalVendas() {
