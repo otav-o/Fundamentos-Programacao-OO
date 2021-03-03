b03b3e6384b3d82630fc7f47db491a168be0d6d4
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-02 22:15:12 -0300

    Criação de adicional para filhos e dedução de INSS

--- src/com/aula2/Funcionario.java
+++ src/com/aula2/Funcionario.java
@@ -10,6 +10,29 @@ public class Funcionario {
         this.totalVendas = totalVendas;
     }
 
+    public double CalcularSalario() {
+        double salario = deduzirInss(salarioBase);
+        salario = adicionalFilhotes(salario);
+
+    }
+
+    private double adicionalFilhotes(double salario) {
+        int numero = numeroFilhos;
+
+        if (numero > 3)
+            numero = 3;
+
+        if (numero > 0)
+            salario += 50 * numero;
+
+        return salario;
+    }
+
+    private double deduzirInss(double salario) {
+        return salario * 0.87;
+    }
+
+
     private boolean trabalhaANoite;
     private double salarioBase;
     private int numeroFilhos;
