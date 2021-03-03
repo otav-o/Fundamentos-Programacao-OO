fe0ec0f6436ab18ffb953171bb1e89c5a21dfcd5
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-02 22:39:54 -0300

    Fim da classe Empresa

--- src/com/aula2/Empresa.java
+++ src/com/aula2/Empresa.java
@@ -2,13 +2,24 @@ package com.aula2;
 
 public class Empresa {
 
-    public Empresa (double totalVendas) {
+    public Empresa (double totalVendas, Funcionario[] funcionarios) {
         this.totalVendas = totalVendas;
+        this.funcionarios = funcionarios;
     }
 
-    private double totalVendas;
+    public double calcularFolhaPagamento() {
+        double total = 0;
+        for (int i = 0; i < funcionarios.length; i++)
+            total += funcionarios[0].CalcularSalario();
+
+        return total;
+    }
+
+    private double totalVendas; // inútil
+
+    private Funcionario[] funcionarios;
 
     public double getTotalVendas() {
         return totalVendas;
-    }
+    } // inútil
 }
