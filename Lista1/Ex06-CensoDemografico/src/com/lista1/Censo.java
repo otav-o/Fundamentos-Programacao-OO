187dc0c1c6a23b763663587294398776689b5fe4
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-03 22:40:56 -0300

    Criação do método para o último comando do exercício

--- Lista1/Ex06-CensoDemografico/src/com/lista1/Censo.java
+++ Lista1/Ex06-CensoDemografico/src/com/lista1/Censo.java
@@ -17,8 +17,17 @@ public class Censo {
         return maisBaixa().altura;
     }
 
-    public double numeroPorSexo(Genero g) {
+    public int numeroPorSexo(Genero g) {
         return pessoasPorSexo(g).size(); // problema de desempenho: vai executar toda vez que for chamado (em vez de guardar o cálculo)
+    }
+
+    public double porcentagemPorSexoIdadeOlhosECabelos(int idadeMin, int idadeMax, Genero g, String corOlhos, String corCabelos) { // horrível!
+        int cont = 0;
+        for (Pessoa p : populacao)
+            if (p.idade >= idadeMin && p.idade <= idadeMax && p.sexo == g && p.corOlhos == corOlhos && p.corCabelos == corCabelos)
+                cont++;
+
+        return cont / populacao.length;
     }
 
     public double mediaAlturaMulheres() {
