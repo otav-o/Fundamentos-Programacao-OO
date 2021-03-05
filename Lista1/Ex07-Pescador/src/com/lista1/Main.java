1b5c00c7cafb5319ccac3df782f42a7f71973ed9
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-05 17:18:37 -0300

    Enunciado

---
+++ Lista1/Ex07-Pescador/src/com/lista1/Main.java
@@ -0,0 +1,17 @@
+/*
+    Um pescador deseja controlar o rendimento diário de seu trabalho. Toda vez que ele
+    traz um peixe maior que o estabelecido pelo regulamento de pesca do Estado de Minas
+    Gerais (50 quilos), deve pagar uma multa de R$4,00 por quilo excedente. Escreva um
+    app que leia as informações dos peixes pegos pelo pescador, ao final mostre quanto o
+    mesmo deve pagar.
+ */
+
+
+package com.lista1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
