104b017f5680b79dd852053f44c1e44e77ce25c8
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-05 17:28:07 -0300

    Criação da classe Peixe

---
+++ Lista1/Ex07-Pescador/src/com/lista1/Peixe.java
@@ -0,0 +1,11 @@
+package com.lista1;
+
+public class Peixe {
+    public Peixe(double peso) {
+        this.peso = peso;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    private double peso;
+}
