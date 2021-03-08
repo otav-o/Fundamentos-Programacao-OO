0bf21894790d682f43ec6119af4196cb8db59103
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-08 15:00:42 -0300

    Mudança de array para list, criação de retornaCandidatos()

--- Lista1/Ex08-EleicaoRepresentante/src/com/lista1/Eleicao.java
+++ Lista1/Ex08-EleicaoRepresentante/src/com/lista1/Eleicao.java
@@ -1,10 +1,24 @@
 package com.lista1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Eleicao {
 
-    public Eleicao (Aluno[] turma) {
+    public Eleicao (List<Aluno> turma) {
         this.turma = turma;
     }
 
-    private Aluno[] turma;
+    public void realizarEleicao() {
+
+    }
+
+    private List<Aluno> retornarCandidatos() {
+        List<Aluno> candidatos = new ArrayList<>();
+        for (Aluno a : turma)
+            if (a.getEhCandidato())
+                candidatos.add(a);
+        return candidatos;
+    }
+
+    private List<Aluno> turma;
 }
