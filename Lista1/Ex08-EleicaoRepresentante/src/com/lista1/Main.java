14995e687719684e3be58cbebd57bbd4226d3b63
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-08 14:49:13 -0300

    Enunciado

---
+++ Lista1/Ex08-EleicaoRepresentante/src/com/lista1/Main.java
@@ -0,0 +1,15 @@
+/*
+    Zezin deseja realizar uma votação para representante de turma, todos os alunos
+    podem ser votados e cada aluno tem direito a um voto. Faça um sistema que permita
+    que os alunos realizem seus votos ao final mostre o nome do aluno ganhador e qual o
+    percentual dos votos.
+ */
+
+package com.lista1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
