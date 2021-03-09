7c3f2ee6afc6e05c51c2048982a320d8fd28934d
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-09 16:23:13 -0300

    Testes iniciais

--- Lista1/Ex03-CompraDeCarro/src/com/lista1/Main.java
+++ Lista1/Ex03-CompraDeCarro/src/com/lista1/Main.java
@@ -1,24 +1,42 @@
 /*
     Crie um aplicativo que permita ao consumidor customizar a escolha de um carro
-    novo com seus opcionais, câmbio automático, vidro automático, alarme, teto solar, kit
-    multimídia, potência do motor, dentre outros opcionais.
+    novo com seus acessórios
 
     Ao final mostre os detalhes do carro escolhido e o custo conforme o cálculo a seguir:
 
-    O preço do carro depende do modelo, acrescido IPI, 20% do custo final do veículo, se
-    for importado ainda é taxado em 30% sobre o preço. Veículos com motores 1.0 o IPI é
-    reduzido a 10%.
-    Cada acessório opcional acrescenta seu custo ao veículo, segue a tabela de preço
-    abaixo:
-    Ar -> R$ 3.000,00, câmbio automático -> R$ 5.000,00, alarme -> R$ 800,00, pintura
-    especial, metálica ou comemorativa -> R$ 2.500,00, teto solar -> R$ 4.000,00 e kit
-    multimidia -> R$ 1.800,00
+    O preço do carro depende do modelo;
+    IPI: 20% do custo final do veículo;
+        Exceção: 10% se o motor for 1.0
+    se for importado, há a taxa de 30% sobre o preço.
+
+    Ar -> R$ 3.000,00;
+    Câmbio automático -> R$ 5.000,00;
+    Alarme -> R$ 800,00;
+    Pintura especial, metálica ou comemorativa -> R$ 2.500,00;
+    teto solar -> R$ 4.000,00;
+    kit multimidia -> R$ 1.800,00
  */
 package com.lista1;
+
+import com.lista1.acessorios.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        var c = gerarCarroCompleto();
+        System.out.println(c.calcularPrecoFinal());
+    }
 
+    private static Carro gerarCarroCompleto() {
+        var c = new Carro();
+        c.adicionarAcessorio(
+                new Alarme(),
+                new ArCondicionado(),
+                new CambioAutomatico(),
+                new KitMultimidia(),
+                EPintura.COMEMORATIVA,
+                new TetoSolar()
+        );
+        return c;
     }
 }
