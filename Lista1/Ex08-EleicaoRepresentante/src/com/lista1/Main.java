0d066561d49db93a7344c4ffcb011ae508cb1d05
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-09 13:45:56 -0300

    Teste do programa

--- Lista1/Ex08-EleicaoRepresentante/src/com/lista1/Main.java
+++ Lista1/Ex08-EleicaoRepresentante/src/com/lista1/Main.java
@@ -6,10 +6,25 @@
  */
 
 package com.lista1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
+        var turma = gerarAlunos();
+        var eleicao2021 = new Eleicao(turma);
+        eleicao2021.iniciarVotacao();
+    }
 
+    private static List<Aluno> gerarAlunos() {
+        ArrayList<Aluno> retorno = new ArrayList<>();
+        retorno.add(new Aluno(1, "Daves"));
+        retorno.add(new Aluno(2, "Miriã"));
+        retorno.add(new Aluno(3, "Camillo"));
+        retorno.add(new Aluno(4, "Gildo"));
+        retorno.add(new Aluno(5, "Camila"));
+        // não existe um jeito mais fácil de acicionar elementos a uma lista? Com chaves por exemplo?
+
+        return retorno;
     }
 }
