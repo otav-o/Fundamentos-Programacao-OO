095208ff3f9f93b91853e9f9e621dd2a4bca27c6
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-09 16:37:57 -0300

    Criação da classe motor

---
+++ Lista1/Ex03-CompraDeCarro/src/com/lista1/Motor.java
@@ -0,0 +1,14 @@
+package com.lista1;
+
+public class Motor {
+
+    public Motor(double cilindradas) {
+        this.cilindradas = cilindradas;
+    }
+
+    public double getCilindradas() {
+        return cilindradas;
+    }
+
+    private double cilindradas;
+}
