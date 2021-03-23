83525db10f0f71091a9647b5507aaad942e7221c
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-23 16:20:00 -0300

    [Fim] Teste das classes no Main

--- Lista2/Ex07-TimeFutebol/src/Main.java
+++ Lista2/Ex07-TimeFutebol/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var t1 = new TimeFutebol("São Paulo", "São Paulo", "SP");
+
+	    var j1 = new Jogador("Daniel Alves", 72, 10, 2);
+	    var j2 = new Jogador("Thiao Volpi", 78, 1, 0);
+
+	    t1.addJogador(j1, j2);
+	    ImprimirJogadores(t1);
+    }
+    private static void ImprimirJogadores(TimeFutebol time) {
+        for (var jogadro : time.getJogadores())
+            System.out.printf("Nome: %s\n" +
+                    "Número: %d\n" +
+                    "Peso: %.2f\n" +
+                    "Posição: %d\n\n",
+                    jogadro.nome, jogadro.getNumero(), jogadro.getPeso(), jogadro.getPosicao());
+        // estranho. Não sabia que protected no Java estendia para o mesmo pacote
     }
 }
