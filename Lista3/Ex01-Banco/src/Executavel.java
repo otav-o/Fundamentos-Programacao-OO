cc9263b646387af75d02e4f7abc4bfb954941d5f
otav-o <otaviocacadordioscanio@gmail.com>
2021-04-23 11:39:56 -0300

    Criar consultarContaMenu() em Executavel
    Falta implementar a transferência

--- Lista3/Ex01-Banco/src/Executavel.java
+++ Lista3/Ex01-Banco/src/Executavel.java
@@ -1,6 +1,7 @@
 import Contas.Conta;
 import Contas.ContaCorrente;
 import Contas.ContaPoupanca;
+import Contas.Imprimivel;
 
 import java.util.Scanner;
 
@@ -10,30 +11,72 @@ public class Executavel {
     public void Executar() {
 
         Banco caixa = new Banco();
+        menu(caixa);
+    }
 
+    private int menu(Banco banco) {
         System.out.println("""
                 Bem vindo! Selecione uma opção:
                 (1) Criar conta
                 (2) Consultar conta
                 (3) Encerrar conta
                 (4) Gerar relatório de todas as contas
+                (5) Sair
                 """);
 
         var resp = s.nextInt();
-        direcionarMenu(resp, caixa);
+        direcionarMenu(resp, banco);
+
+        if (resp != 5) resp = menu(banco);
+        return resp;
     }
 
     private void direcionarMenu(int resp, Banco b) {
         switch (resp) {
             case 1: criarContaMenu(b); break;
-            /*
             case 2: consultarContaMenu(b); break;
+            /*
             case 3: excluirContaMenu(b); break;
             case 4: relatorioBanco(b); break;
             */
             default:
         }
     }
+
+    private void consultarContaMenu(Banco b) {
+        System.out.println("Insira o número da conta: ");
+        var resp = s.nextInt();
+        var conta = b.procurarConta(resp);
+        // verificar se conta existe
+        System.out.println("Conta encontrada:\n" + ((Imprimivel) conta).mostrarDados());
+
+        menuConta(conta);
+
+    }
+
+    private void menuConta(Conta conta) {
+        System.out.printf("""
+                Escolha uma operação com a conta %d
+                (a) Depositar
+                (b) Sacar
+                (c) Transferir
+                """, conta.getNumero());
+
+        var resp = s.next();
+
+        System.out.print("Qual o valor? ");
+        var valor = s.nextDouble();
+
+        switch (resp) {
+            case "a": conta.depositar(valor); break;
+            case "b": conta.sacar(valor); break;
+            // case "c":
+            default:
+        }
+    }
+
+
+
 
     private void criarContaMenu(Banco b) {
 
