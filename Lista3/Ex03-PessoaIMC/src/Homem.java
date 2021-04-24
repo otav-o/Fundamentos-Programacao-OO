44704e90d3e6f61d9b2e215cc8f5851302040368
otav-o <otaviocacadordioscanio@gmail.com>
2021-04-24 16:56:17 -0300

    Criar classes Homem e Mulher e implementar resultIMC()

---
+++ Lista3/Ex03-PessoaIMC/src/Homem.java
@@ -0,0 +1,15 @@
+public class Homem extends PessoaIMC {
+    @Override
+    public String resultIMC() {
+        double imc = calculaIMC();
+
+        if (imc <= 20.7)
+            return "Abaixo do peso";
+        else if (20.7 < imc && imc <= 26.4)
+            return "Peso ideal";
+        else if (26.4 < imc)
+            return "Sobrepeso";
+        else
+            return null;
+    }
+}
---
+++ Lista3/Ex03-PessoaIMC/src/Mulher.java
@@ -0,0 +1,15 @@
+public class Mulher extends PessoaIMC {
+    @Override
+    public String resultIMC() {
+        double imc = calculaIMC();
+
+        if (imc <= 19)
+            return "Abaixo do peso";
+        else if (19 < imc && imc <= 25.8)
+            return "Peso ideal";
+        else if (25.8 < imc)
+            return "Sobrepeso";
+        else
+            return null;
+    }
+}
