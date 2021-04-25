37b13a9d5aa3a9e367931dceaff5e642bc2694ef
otav-o <otaviocacadordioscanio@gmail.com>
2021-04-25 19:09:16 -0300

    Adicionar construtor a Bicicleta

--- Lista3/Ex05-Bicicleta/src/Bicicleta/Bicicleta.java
+++ Lista3/Ex05-Bicicleta/src/Bicicleta/Bicicleta.java
@@ -1,15 +1,28 @@
 package Bicicleta;
 
 import Bicicleta.Acessorios.IAcessorio;
+import Bicicleta.Pecas.Banco;
+import Bicicleta.Pecas.Corrente;
 import Bicicleta.Pecas.IPeca;
+import Bicicleta.Pecas.Roda;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bicicleta implements IPreco {
 
-    public void addAcessorio(IAcessorio acessorio) {
-        acessorios.add(acessorio);
+    public Bicicleta(int aro, int tamanhoBanco, int maciezBanco, double espessuraCorrente) {
+        addPeca(
+                new Roda(aro),
+                new Roda(aro), 
+                new Banco(tamanhoBanco, maciezBanco),
+                new Corrente(espessuraCorrente)
+        );
+    }
+
+    public void addAcessorio(IAcessorio... acessorio) {
+        for (var a : acessorio)
+            acessorios.add(a);
     }
 
     @Override
@@ -20,6 +33,11 @@ public class Bicicleta implements IPreco {
         for (IPreco objeto : pecas)
             total += objeto.getPreco();
         return total;
+    }
+
+    private void addPeca(IPeca... pecas) {
+        for (var p : pecas)
+            this.pecas.add(p);
     }
 
     private List<IPeca> pecas = new ArrayList<>();
